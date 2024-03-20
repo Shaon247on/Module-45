4d7462fce094aae535d14118a1192d693dc3fe0d
shaon <aminul007.ais@gmail.com>
2024-03-20 15:25:29 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <h2>NavBar</h2>
+            <nav className="space-x-5">
+                <a href="/">Home</a><a href="/about">About</a><a href="/contact">Contact</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
